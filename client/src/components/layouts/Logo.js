190a1084c84d64ff5dd6f6b3f47f957ab475d62c
Christopher DiStasio <cadistasio@gmail.com>
2021-04-24 13:55:28 -0500

    Removes logo file; replaces with AWS S3 url

--- client/src/components/layouts/Logo.js
+++ client/src/components/layouts/Logo.js
@@ -1,11 +1,10 @@
 /* eslint-disable react/display-name */
 import React from 'react';
-import logo from './mat-logo.png';
 
 export default () => (
   <>
     <img
-      src={logo}
+      src="https://dreamsourcer-assets.s3.amazonaws.com/mat-logo.png"
       style={{ width: '50px', margin: 'auto', display: 'block' }}
       alt='Mobile App Template Logo'
     />
