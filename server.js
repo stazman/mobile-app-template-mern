a49e83f56911a533c26ea96071531e87c0546419
Christopher DiStasio <cadistasio@gmail.com>
2021-04-24 23:17:05 -0500

    Prepares for production; adds static assets

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 4000;
+const path = require('path');
 
 connectDB();
 
@@ -12,11 +13,22 @@ app.use(express.json({
   extended: false
 }));
 
-app.get('/', (req, res) => res.send('API running'));
+// app.get('/', (req, res) => res.send('API running'));
 
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//Serve Static Assets in production
+
+if(process.env.NODE_ENV === 'production'){
+
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, function(){
   console.log('Server is running on Port ' + PORT);
